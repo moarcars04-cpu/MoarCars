c19316df10c33b6ae988cef083c17c4107c3b4bb
Pandu1712 <prasannakumarpanda2000@gmail.com>
2026-08-31 15:17:59 +0530

    Update root server.js launcher to use direct ES module import instead of spawning child process to satisfy Phusion Passenger on Hostinger

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-const { spawn } = require("child_process");
-const path = require("path");
-
-console.log("Starting unified Node/Express server on Hostinger's assigned port...");
-const backend = spawn("node", [path.join(__dirname, "backend", "server.js")], {
-  stdio: "inherit",
-  env: process.env, // This inherits Hostinger's assigned PORT
-});
-
-backend.on("exit", (code) => {
-  console.log(`Backend process exited with code ${code}`);
-});
+import "./backend/server.js";
